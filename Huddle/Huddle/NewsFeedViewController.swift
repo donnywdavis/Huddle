33a645b9619e8fbc40a356ff20403367ad5db003
Donny Davis <donnywdavis@icloud.com>
2016-06-01 17:50:37 -0400

    Create view controller for the news feed

---
+++ Huddle/Huddle/NewsFeedViewController.swift
@@ -0,0 +1,19 @@
+//
+//  NewsFeedViewController.swift
+//  Huddle
+//
+//  Created by Donny Davis on 6/1/16.
+//  Copyright © 2016 Donny Davis. All rights reserved.
+//
+
+import UIKit
+
+class NewsFeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
